8c505cde3813b23694c54a61b0a46bd295bba8fb
Le Bris Corentin <corentin.lebris@hotmail.fr>
2022-05-18 16:46:17 +0200

    put the form inside the show review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,12 +15,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  def destroy
-    @review = Review.find(params[:id])
-    @review.destroy
-    redirect_to restaurant_path(@review.restaurant)
-  end
-
   private
 
   def review_params
