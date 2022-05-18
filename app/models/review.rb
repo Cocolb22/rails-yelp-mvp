2641e9d4d133a2411b3b344a99ba27255357beb8
Le Bris Corentin <corentin.lebris@hotmail.fr>
2022-05-18 16:29:33 +0200

    create a two models app

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: 0..5 }, presence: true
+  validates :content, presence: true
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dishoom = Restaurant.create(name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese")
+pizza_east = Restaurant.create(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian")
+monster_burger = Restaurant.create(name: "Monster Burger", address: "22 rue du stade, 22000 Saint-Brieuc", category: "belgian")
+pasta_box = Restaurant.create(name: "Pasta Box", address: "1170, rue de la révolution Beijing", category: "french")
+sushi_world = Restaurant.create(name: "Sushi World", address: "esplanade de la morue, Tokyo", category: "japanese")
